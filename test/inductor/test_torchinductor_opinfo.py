ae6505baaf286bee663311c9d0c09ef745f83553
--help <lukas.geiger94@gmail.com>
2026-06-19 10:49:25 +0100

    Remove expected inductor test failure for torch.normal

--- test/inductor/test_torchinductor_opinfo.py
+++ test/inductor/test_torchinductor_opinfo.py
@@ -250,8 +250,6 @@ inductor_expected_failures_single_sample["cpu"] = {
     "histc": {f16},
     "nonzero_static": {b8, f16, f32, f64, i32, i64},
     ("normal", "in_place"): {f16, f32, f64},
-    ("normal", "number_mean"): {f16, f32, f64},
-    "normal": {f16, f32, f64},
     ("sparse.mm", "reduce"): {f32, f64, f16},
     "sparse.sampled_addmm": {f32, f64},
     "to_sparse": {
@@ -265,8 +263,6 @@ inductor_expected_failures_single_sample["cpu"] = {
 inductor_expected_failures_single_sample["cuda"] = {
     "_upsample_bilinear2d_aa": {f16, f32, f64},
     ("normal", "in_place"): {f16, f32, f64},
-    ("normal", "number_mean"): {f16, f32, f64},
-    "normal": {f16, f32, f64},
     "sparse.sampled_addmm": {f32, f64},
     "torch.ops.aten._flash_attention_forward": {f16},
     "torch.ops.aten._efficient_attention_forward": {f16, f32},
@@ -283,8 +279,6 @@ inductor_expected_failures_single_sample["cuda"] = {
 inductor_expected_failures_single_sample["xpu"] = {
     "_upsample_bilinear2d_aa": {f16, f32, f64},
     ("normal", "in_place"): {f16, f32, f64},
-    ("normal", "number_mean"): {f16, f32, f64},
-    "normal": {f16, f32, f64},
     "sparse.sampled_addmm": {f32, f64},
     "tan": {f16},
     "torch.ops.aten._flash_attention_forward": {f16},
